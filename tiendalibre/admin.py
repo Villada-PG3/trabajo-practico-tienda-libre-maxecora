947e5cbf6612352d537b5051988409ca12b861e5
MAXIMO <m.corallo.margosian@itsv.edu.ar>
2026-07-27 14:37:15 +0000

    actualizaciones

--- tiendalibre/admin.py
+++ tiendalibre/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Producto
+from .models import Categoria
 admin.site.register(Producto)
+admin.site.register(Categoria)
--- tiendalibre/models.py
+++ tiendalibre/models.py
@@ -1,11 +1,33 @@
 from django.db import models
 
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length= 100, unique=True)
+    #slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = 'categoria'
+        verbose_name_plural = 'categorias'
+        ordering = ['nombre']
 # Create your models here.
 class Producto(models.Model):
+    categoria = models.ForeignKey(
+        Categoria, 
+        on_delete=models.CASCADE,
+        related_name='productos',
+        null=True,
+        blank=True
+    )
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
+    marca = models.CharField(max_length=100, default='Marca Desconocida')
+    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
+    class Meta:
+        verbose_name = 'producto'
+        verbose_name_plural = 'productos'
+        ordering = ['nombre']
 
     def __str__(self):
         return self.nombre
