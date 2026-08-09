c882ddcd9ffabbc4af089dd76c52cc9a50795196
MAXIMO <m.corallo.margosian@itsv.edu.ar>
2026-08-09 21:34:39 +0000

    primera vista y template para home y acerca-de-mi

--- tiendalibre/urls.py
+++ tiendalibre/urls.py
@@ -6,4 +6,5 @@ app_name = 'tiendalibre'
 urlpatterns = [
     path('', views.home, name='home'),
     path('home_1/', views.home_1, name='home_1'),
+    path('acerca-de-mi/', views.acerca_de_mi, name='acerca-de-mi'),
 ]
--- tiendalibre/views.py
+++ tiendalibre/views.py
@@ -7,7 +7,10 @@ class TiendaTemplateView(TemplateView):
     template_name = 'tienda.html'
 
 def home_1(request):
-    return HttpResponse("<h1>Bienvenido a la tienda Libre</h1>")
+    return HttpResponse("<h1>Bienvenido a la tienda Libre de Maximo Corallo Margosian</h1>")
 
 def home(request):
     return render(request, 'home.html')
+
+def acerca_de_mi(request):
+    return render(request, 'acerca-de-mi.html')
