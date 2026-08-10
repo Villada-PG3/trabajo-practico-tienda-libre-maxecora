4262c481e303cf9c99b027b48014e068aa042042
MAXIMO <m.corallo.margosian@itsv.edu.ar>
2026-08-10 13:19:08 +0000

    previsualizacion de imagenes el dia 10/8/26

--- tiendalibre/admin.py
+++ tiendalibre/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.utils.html import format_html
 from .models import Producto, Categoria
-admin.site.register(Producto)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'mostrar_miniatura')
+    readonly_fields = ('mostrar_imagen_detalle',)
+
+    def mostrar_miniatura(self, obj):
+        if obj.imagen:
+            return format_html('<img src="{}" style="width: 50px; height: 50px; object-fit: cover;" />', obj.imagen.url)
+        return "No hay imagen"
+
+    mostrar_miniatura.short_description = 'Miniatura'
+
+    def mostrar_imagen_detalle(self, obj):
+        if obj.imagen:
+            return format_html('<img src="{}" style="width: 100px; height: 100px; object-fit: cover;" />', obj.imagen.url)
+        return "No hay imagen"
+
+    mostrar_imagen_detalle.short_description = 'Previsualización de la imagen'
+
 admin.site.register(Categoria)
+
