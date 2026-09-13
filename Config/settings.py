d38d1cec436cd522875885c52fa2aeebbcc41a28
MAXIMO <m.corallo.margosian@itsv.edu.ar>
2026-09-13 22:17:17 +0000

    página de detalle de producto con URL dinámica.

--- Config/settings.py
+++ Config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o_243f8mau5)&v^%ub^1!&!iy^sz(j!%po$064%u6nya_qwn#m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
